24db5e28f1d2d2fb558ea1f7094ca048deb418c0
KaleKemosabe <kale.kemosabe@gmail.com>
2021-09-25 15:47:01 +0300

    Layout, header and footer

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,11 @@
+import Layout from '../components/Layout';
+
 export default function AboutPage() {
     return (
-        <div>
+        <Layout title="About B2B events">
             <h1>About</h1>
             <p>This app helps you find some cool events</p>
             <p>version 1.0</p>
-        </div>
+        </Layout>
     )
 }
