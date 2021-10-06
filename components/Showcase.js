f9625c96517c110a34bfca05e2aeebfb156f495a
KaleKemosabe <kale.kemosabe@gmail.com>
2021-10-06 19:08:40 +0300

    api routes before strapi setup

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+    return (
+        <div className={styles.showcase}>
+            <h1>Go and socialize!</h1>
+            <h2>Find some new conferences to attend.</h2>
+        </div>
+    )
+}
