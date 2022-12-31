452be9c18953427a3d595bab21a13494edfcee2b
KaleKemosabe <kale.kemosabe@gmail.com>
2022-12-31 15:16:56 +0200

    Create Modal component for uploading image

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,84 +1,99 @@
-import { FaPencilAlt, FaTimes } from 'react-icons/fa'
-import Link from 'next/link';
-import Image from 'next/image';
-import Layout from '@/components/Layout';
-import { API_URL } from '@/config/index';
-import styles from '@/styles/Event.module.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { FaPencilAlt, FaTimes } from "react-icons/fa";
+import Link from "next/link";
+import Image from "next/image";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import styles from "@/styles/Event.module.css";
+import { useRouter } from "next/router";
 
-export default function EventPage({evt}) {
-    const deleteEvent = (e) => {
-        console.log('delete')
+export default function EventPage({ evt }) {
+  const router = useRouter();
+
+  const deleteEvent = async (e) => {
+    if (confirm("Are you sure?")) {
+      const res = await fetch(`${API_URL}/events/${evt.id}`, {
+        method: "DELETE",
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        toast.error(data.message);
+      } else {
+        router.push("/events");
+      }
     }
+  };
 
-    return (
-        <Layout>
-            <div className={styles.event}>
-                <div className={styles.controls}>
-                    <Link href={`/events/edit/${evt.id}`}>
-                        <a>
-                            <FaPencilAlt /> Edit Event
-                        </a>
-                    </Link>
-                    <a href="#" className={styles.delete} onClick={deleteEvent}>
-                        <FaTimes /> Delete Event
-                    </a>
-                </div>
+  return (
+    <Layout>
+      <div className={styles.event}>
+        <div className={styles.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href="#" className={styles.delete} onClick={deleteEvent}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
 
-                <span>
-                    {new Date(evt.date).toLocaleDateString ('en-GB')} at {evt.time}
-                </span>
-                <h1>{evt.name}</h1>
-                {evt.image && (
-                    <div className={styles.image}>
-                        <Image
-                            src={evt.image.formats.medium.url}
-                            width={960}
-                            height={600}
-                            alt=""
-                        />        
-                    </div>
-                )}
+        <span>
+          {new Date(evt.date).toLocaleDateString("en-GB")} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        <ToastContainer />
+        {evt.image && (
+          <div className={styles.image}>
+            <Image
+              src={evt.image.formats.medium.url}
+              width={960}
+              height={600}
+              alt=""
+            />
+          </div>
+        )}
 
-                <h3>Speakers:</h3>
-                <p>{evt.performers}</p>
-                <h3>Description:</h3>
-                <p>{evt.description}</p>
-                <h3>Venue:</h3>
-                <p>{evt.address}</p>
+        <h3>Speakers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue:</h3>
+        <p>{evt.address}</p>
 
-                <Link href='/events'>
-                    <a className={styles.back}>
-                        {'<'} Go back
-                    </a>
-                </Link>
-            </div>
-        </Layout>
-    )
+        <Link href="/events">
+          <a className={styles.back}>{"<"} Go back</a>
+        </Link>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
-    const res = await fetch(`${API_URL}/events`)
-    const events = await res.json()
+  const res = await fetch(`${API_URL}/events`);
+  const events = await res.json();
 
-    const paths = events.map(evt => ({
-        params: {slug: evt.slug}
-    }))
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug },
+  }));
 
-    return {
-        paths,
-        fallback: true,
-    }
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
-export async function getStaticProps({params: {slug}}) {
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/events?slug=${slug}`);
+  const events = await res.json();
 
-    const res = await fetch(`${API_URL}/events?slug=${slug}`)
-    const events = await res.json()
-
-    return {
-        props: {
-            evt: events[0],
-        },
-        revalidate: 1
-    }
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1,
+  };
 }
